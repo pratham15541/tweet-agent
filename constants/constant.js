0e323efcbcffc811710d844f090ab26b794dae3e
pratham15541 <prathamparikh94@gmail.com>
2025-07-20 14:07:33 +0530

    Initial Commit

---
+++ constants/constant.js
@@ -0,0 +1,8 @@
+import 'dotenv/config'
+
+export const GEMINI_API_KEY = process.env.GEMINI_API_KEY
+export const TWITTER_ACCESS_TOKEN = process.env.TWITTER_ACCESS_TOKEN;
+export const TWITTER_ACCESS_TOKEN_SECRET = process.env.TWITTER_ACCESS_TOKEN_SECRET;
+export const TWITTER_API_KEY = process.env.TWITTER_API_KEY;
+export const TWITTER_API_SECRET = process.env.TWITTER_API_SECRET;
+export const SERPER_KEY = process.env.SERPER_API_KEY; // 10k/mo free
